a9acbe698ba346f9ca2209ca2a394368fb5e8510
Duarte de Freitas <durtoff@gmail.com>
2019-07-19 13:09:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,23 @@ function add(a, b) {
   add = a + b
   return add
 }
+
+function subtract(a, b) {
+  add = a - b
+  return add
+}
+
+function multiply(a, b) {
+  add = a * b
+  return add
+}
+
+function divide(a, b) {
+  add = a / b
+  return add
+}
+
+function inc(n) {
+  inc = + n
+  return inc
+}
